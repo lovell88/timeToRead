519ca02fa606f6ad89e3cc52a6c4596c2197882d
lovell88 <49254089+lovell88@users.noreply.github.com>
2019-04-06 12:06:29 -0600

    Add files via upload

--- WordsPerMinute.java
+++ WordsPerMinute.java
@@ -1,4 +1,24 @@
 
 public class WordsPerMinute {
 
+	public void Paragraph () {
+		
+		/* TODO
+		 * Read paragraph from file and display on console. Instruct user to time themselves
+		 */
+		return;
+	}
+	
+	public double wPM () {
+		
+		double wordsPerMinute = 0;
+		
+		/* TODO 
+		 * Have the user enter how many seconds it took to read it, and then calculate file
+		 */
+		
+		return wordsPerMinute;
+	}
+	
+	
 }
